e3bdf16d55f9f1e036571b19b1fc6ecbb5527bb9
GitHub Action <action@github.com>
2022-12-04 04:00:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/swimminghacker/gopaas/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swimminghacker/gopaas/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swimminghacker.gopaas.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swimminghacker.gopaas.MyBundle
 
 class MyProjectService(project: Project) {
 
